fc31bae18c3bb581a1f882f746aa1cee3dbd9f54
urmil <ucodes28@gmail.com>
2020-09-07 10:48:51 -0500

    Built contacts app UI

---
+++ contactsapp/ContactsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ContactsViewController.swift
+//  contactsapp
+//
+//  Created by Urmil Shah on 9/7/20.
+//  Copyright © 2020 Spark28 Apps. All rights reserved.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+
+    ///MARK - Outlets
+    
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+        ///MARK: -  IBAction
+
+    @IBAction func addBarButtonPressed(_ sender: Any) {
+    }
+    
+
+}
