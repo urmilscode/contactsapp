109999d285307afa9e1e8c5fb9d00ba40566d647
urmil <ucodes28@gmail.com>
2020-09-07 11:17:14 -0500

    added tableview data source methodsand alert controllers on bar button tap

--- contactsapp/ContactsViewController.swift
+++ contactsapp/ContactsViewController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
-class ContactsViewController: UIViewController {
+class ContactsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
 
+    struct Contact {
+        var name: String
+        var surname: String
+        var phoneNumber: Int
+    }
+    
+    
+    ///Contacts array holding the contact objects
+    
+    var allContacts: [Contact] = []
+    ///var allContacts = [Contact]()
+    
     ///MARK - Outlets
-    
-    
-    
-    
     @IBOutlet weak var tableView: UITableView!
     
     
@@ -27,10 +36,58 @@ class ContactsViewController: UIViewController {
     }
     
     
+    ///MARK: Table View Data Source
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+    
+    
         ///MARK: -  IBAction
 
     @IBAction func addBarButtonPressed(_ sender: Any) {
+        print("Add new Item")
+        showAlertController()
+        
     }
+    
+    
+    ///MARK: Helpers
+    
+    
+    func showAlertController() {
+        
+        
+        ///Alert Controller
+        
+        let alertController = UIAlertController(title: "Create New Contact", message: nil, preferredStyle: .alert)
+        
+        
+        ///Save and Cancel Actions
+        
+        let saveButton = UIAlertAction(title: "Save", style: .default) { (action) in
+            
+        }
+        
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+            
+        }
+        
+        
+        alertController.addAction(saveButton)
+        alertController.addAction(cancelButton)
+        
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
     
 
 }
